06be9aca3e60c7c8b7d96b27a0417e13938206be
Burt Beckwith <burt@burtbeckwith.com>
2013-04-21 00:25:16 -0400

    0.4.4 release docs

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 grails.project.work.dir = 'target'
+grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
 grails.project.dependency.resolution = {
 
