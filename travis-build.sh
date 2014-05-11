67ed93c5d46655456855e320937cda49a424224d
graemerocher <graeme.rocher@gmail.com>
2014-05-11 12:07:49 +0200

    fix repo slug

--- travis-build.sh
+++ travis-build.sh
@@ -16,7 +16,7 @@ plugin=${plugin/"-$version"/}
 
 echo "Publishing plugin $plugin with version $version"
 
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "grails-plugins/grails-$plugin" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "gpc/grails-$plugin" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   git config --global user.name "$GIT_NAME"
   git config --global user.email "$GIT_EMAIL"
   git config --global credential.helper "store --file=~/.git-credentials"
