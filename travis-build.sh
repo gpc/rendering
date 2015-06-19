6a29e4a76b3dcf3798127a84209b01b0533fc63f
Graeme Rocher <graeme.rocher@gmail.com>
2015-06-19 17:15:28 +0100

    Initial upgrade to Grails 3.0

--- travis-build.sh
+++ travis-build.sh
@@ -1,48 +1,66 @@
 #!/bin/bash
 set -e
 rm -rf *.zip
-./grailsw refresh-dependencies --non-interactive
-./grailsw test-app --non-interactive
-./grailsw package-plugin --non-interactive
-./grailsw doc --pdf --non-interactive
+./gradlew clean test assemble
 
-filename=$(find . -name "grails-*.zip" | head -1)
-filename=$(basename $filename)
-plugin=${filename:7}
-plugin=${plugin/.zip/}
-plugin=${plugin/-SNAPSHOT/}
-version="${plugin#*-}"; 
-plugin=${plugin/"-$version"/}
+filename=$(find build/libs -name "*.jar" | head -1)
+filename=$(basename "$filename")
 
-echo "Publishing plugin $plugin with version $version"
+EXIT_STATUS=0
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "gpc/grails-$plugin" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+  echo "Publishing archives"
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  else
+      ./gradlew publish || EXIT_STATUS=$?
+  fi
+
+  ./gradlew docs || EXIT_STATUS=$?
+
   git config --global user.name "$GIT_NAME"
   git config --global user.email "$GIT_EMAIL"
   git config --global credential.helper "store --file=~/.git-credentials"
   echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
+  git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git -b gh-pages gh-pages --single-branch > /dev/null
+  cd gh-pages
 
-  if [[ $filename != *-SNAPSHOT* ]]
-  then
-    git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
-    cd gh-pages
-    git rm -rf .
-    cp -r ../target/docs/. ./
-    git add *
+  # If this is the master branch then update the snapshot
+  if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    mkdir -p snapshot
+    cp -r ../build/docs/manual/. ./snapshot/
+
+    git add snapshot/*
+  fi
+
+    # If there is a tag present then this becomes the latest
+    if [[ -n $TRAVIS_TAG ]]; then
+        mkdir -p latest
+        cp -r ../build/docs/manual/. ./latest/
+        git add latest/*
+
+        version="$TRAVIS_TAG"
+        version=${version:1}
+        majorVersion=${version:0:4}
+        majorVersion="${majorVersion}x"
+
+        mkdir -p "$version"
+        cp -r ../build/docs/manual/. "./$version/"
+        git add "$version/*"
+
+        mkdir -p "$majorVersion"
+        cp -r ../build/docs/manual/. "./$majorVersion/"
+        git add "$majorVersion/*"
+
+    fi
+
     git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
     git push origin HEAD
     cd ..
     rm -rf gh-pages
-  else
-    echo "SNAPSHOT version, not publishing docs"
-  fi
-
-
-  ./grailsw publish-plugin --no-scm --allow-overwrite --non-interactive
-else
-  echo "Not on master branch, so not publishing"
-  echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-  echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
-  echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 fi
+
+exit $EXIT_STATUS
